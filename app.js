60ac8b607376b87abf55916bd0c65eb7c198da0d
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-01 13:08:49 +0600

    Setting up the First Event Listeners
    keypress
    button

--- app.js
+++ app.js
@@ -40,37 +40,43 @@
 
 
 
-// 1st module created
+// Budget Controller
 var budgetController = (function(){
-   var x = 23;
-   var add = function(a){
-   	return x + a;
-   }
-   return{
-   	publicTest: function(b){
-   		return add(b);
-   	}
-   }
+
 })();
 
 
-//2nd module (ui)
+// UI Controller
 var UIController = (function(){
 
 })();
 
 
-//another module (controller module)
-//this module connect 1st & 2nd module
-//this module take 1st & 2nd module as arguments
-//1st & 2nd module ke arguments hishebe pass na koreo use kora jabe but not good practice
+// Global App Controller
 
 var controller = (function(budgetCtrl, UICtrl){
-	var z =budgetCtrl.publicTest(5);
-	return {
-		anotherPublic : function(){
-			console.log(z);
-		}
+
+	var ctrlAddItem = function(){
+		// 1. Get the field input data
+		// 2.Add the item to the Budget Controller
+		// 3.Add the item to the UI
+		// 4.Calculate the budget
+		// 5.Display the budget on the UI	
+
+		console.log('works!!');	
+
 	}
+
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+
+	document.addEventListener('keypress', function(event){
+		if (event.keyCode=== 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+
+	});
+
 
 })(budgetController, UIController);
