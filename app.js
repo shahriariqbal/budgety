25ea53b39611ced08e537ac5d4e84c8dada4b5f2
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-01 18:04:22 +0600

    Creating an Initialization Function

--- app.js
+++ app.js
@@ -51,29 +51,24 @@ var budgetController = (function(){
 
 // UI Controller
 var UIController = (function(){
-
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn'
     };
-
-	return{
-		getInput: function(){
-
-			return{
+    return{
+     getInput: function(){
+        return{
 			type : document.querySelector(DOMstrings.inputType).value,
 			description : document.querySelector(DOMstrings.inputDescription).value,
 			value : document.querySelector(DOMstrings.inputValue).value				
 			};
-
-		},
-		getDOMstrings: function(){
+        },
+	 getDOMstrings: function(){
 			return DOMstrings;
 		}
 	}
-
 })();
 
 
@@ -84,32 +79,33 @@ var UIController = (function(){
 // Global App Controller
 
 var controller = (function(budgetCtrl, UICtrl){
-
+  var setupEventListeners = function(){
 	var DOM = UICtrl.getDOMstrings();
-
-	var ctrlAddItem = function(){
-		// 1. Get the field input data
-		var input = UICtrl.getInput();
-		console.log(input);
-		// 2.Add the item to the Budget Controller
-		// 3.Add the item to the UI
-		// 4.Calculate the budget
-		// 5.Display the budget on the UI	
-
-		
-
-	}
-
-
 	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-
-	document.addEventListener('keypress', function(event){
+    document.addEventListener('keypress', function(event){
 		if (event.keyCode=== 13 || event.which === 13) {
 			ctrlAddItem();
 		}
-
 	});
+  }
+  var ctrlAddItem = function(){
 
+	// 1. Get the field input data
+	var input = UICtrl.getInput();
+		
+	// 2.Add the item to the Budget Controller
+	// 3.Add the item to the UI
+	// 4.Calculate the budget
+	// 5.Display the budget on the UI	
 
+   };
+
+   return {
+	init: function(){
+		console.log('Application has started');
+		setupEventListeners();
+        }
+	};
 })(budgetController, UIController);
+
+controller.init();
