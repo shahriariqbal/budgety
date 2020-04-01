d3419f57d5d7e5f6b46bab611765d8f89bc7de0c
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-01 20:18:02 +0600

    Creating Income and Expense Function Constructors

--- app.js
+++ app.js
@@ -43,7 +43,32 @@
 // Budget Controller
 var budgetController = (function(){
 
+	var Expense = function(id, description, value){
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var Income = function(id, description, value){
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var data = {
+		allItems:{
+			exp: [],
+			inc:[]
+		},
+		totals: {
+			exp: 0,
+			inc: 0
+		}
+	}
+
 })();
+
+
 
 
 
