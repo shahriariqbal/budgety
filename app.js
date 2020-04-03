6171f737e3bd35b8ccfa29b6e701da0448621366
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-03 18:32:34 +0600

    Displaying the Current Month and Year

--- app.js
+++ app.js
@@ -181,6 +181,16 @@ var budgetController = (function(){
 
 
 
+
+
+
+
+
+
+
+
+
+
 // UI Controller
 var UIController = (function(){
 	var DOMstrings = {
@@ -195,7 +205,8 @@ var UIController = (function(){
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
-		expensesPercLabel: '.item__percentage'
+		expensesPercLabel: '.item__percentage',
+		dateLabel: '.budget__title--month'
 	};
 
         var formatNumber = function(num, type){
@@ -308,12 +319,32 @@ var UIController = (function(){
 
         },
 
+        displayMonth: function() {
+        	var now, months, month, year;
+        	now = new Date();
+        	months = ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        	month = now.getMonth();
+        	year = now.getFullYear();
+        	document.querySelector(DOMstrings.dateLabel).textContent =months[month] + ' ' + year;
+
+        },
+
 
 	 getDOMstrings: function(){
 			return DOMstrings;
 		}
 	};
 })();
+
+
+
+
+
+
+
+
+
 
 
 
@@ -404,6 +435,7 @@ var ctrlDeleteItem = function(event){
 return {
 	init: function(){
 		console.log('Application has started');
+		UICtrl.displayMonth();
 		UICtrl.displayBudget({
 			budget: 0,
 			totalInc: 0,
